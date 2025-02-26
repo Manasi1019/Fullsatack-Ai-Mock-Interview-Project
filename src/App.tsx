8650dc5908858794347e8a087c6f08af39e09220
Manasi1019 <manasipatil102021@gmail.com>
2025-02-26 18:08:47 +0530

    Changes: Project Initiated and Setup Completed with ShadCn Ui

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+ import React from 'react';
+import { Button } from './components/ui/button';
+
+ const App = () => {
+   return (
+     <div className='text-blue-500'>App</div>
+   )
+ }
+ 
+ export default App
